e756f75f25886ad22b8e5eeeb2b5663801e86003
nmogylchuk <nataliia.mogylchuk@gmail.com>
2020-01-10 14:21:52 +0200

    Add lots json/js card

---
+++ script/lots.js
@@ -0,0 +1,223 @@
+  const lotsData =  [{
+    "id": 1,
+    "maker": "Volvo",
+    "model": "V40",
+    "price": 19000.00,
+    "currency": "EUR",
+    "year": 2013,
+    "colour": "Синій",
+    "engine": "2.0 Дизель",
+    "gearbox": "Автомат",
+    "mileage": 120000,
+    "mileageUnit": "km",
+    "srcImg": "img/lots/lot_22122019/lot_221219_01.jpg"
+},
+{
+    "id": 2,
+    "maker": "Ford",
+    "model": "Focus",
+    "price": 6000.00,
+    "currency": "EUR",
+    "year": 2013,
+    "colour": "Зелений",
+    "engine": "1.5 Дизель",
+    "gearbox": "Автомат",
+    "mileage": 199000,
+    "mileageUnit": "km",
+    "srcImg": "img/lots/lot_01012020/lot_01012020_01.jpg"
+},
+{
+    "id": 3,
+    "maker": "Jaguar",
+    "model": "XF",
+    "price": 33800.00,
+    "currency": "EUR",
+    "year": 2017,
+    "colour": "Сірий",
+    "engine": "2.0 Бензин",
+    "gearbox": "Автомат",
+    "mileage": 15400,
+    "mileageUnit": "km",
+    "srcImg": "img/lots/lot_02012020/lot_02012020_01.jpg"
+},
+{
+    "id": 4,
+    "maker": "Ford",
+    "model": "Ranger",
+    "price": 33471.00,
+    "currency": "EUR",
+    "year": 2017,
+    "colour": "Білий",
+    "engine": "3.2 Дизель",
+    "gearbox": "Механіка",
+    "mileage": 44300,
+    "mileageUnit": "km",
+    "srcImg": "img/lots/lot_03012020/lot_03012020_01.jpg"
+},
+{
+    "id": 5,
+    "maker": "Ford",
+    "model": "Kuga",
+    "price": 18736.00,
+    "currency": "EUR",
+    "year": 2016,
+    "colour": "Синій",
+    "engine": "2.0 Дизель",
+    "gearbox": "Автомат",
+    "mileage": 52446,
+    "mileageUnit": "km",
+    "srcImg": "img/lots/lot_04012020/lot_04012020_01.jpg"
+},
+{
+    "id": 6,
+    "maker": "Porsche",
+    "model": "Macan",
+    "price": 32600.00,
+    "currency": "EUR",
+    "year": 2016,
+    "colour": "Бежевий",
+    "engine": "2.0 Бензин",
+    "gearbox": "Автомат",
+    "mileage": 30700,
+    "mileageUnit": "km",
+    "srcImg": "img/lots/lot_05012020/lot_05012020_01.jpg"
+},
+{
+    "id": 7,
+    "maker": "Range Rover",
+    "model": "Evoque",
+    "price": 39350.00,
+    "currency": "EUR",
+    "year": 2019,
+    "colour": "Білий",
+    "engine": "2.0 Дизель",
+    "gearbox": "Автомат",
+    "mileage": 10410,
+    "mileageUnit": "km",
+    "srcImg": "img/lots/lot_06012020/lot_06012020_01.jpg"
+},
+{
+    "id": 8,
+    "maker": "Volvo",
+    "model": "V40",
+    "price": 19000.00,
+    "currency": "EUR",
+    "year": 2013,
+    "colour": "Синій",
+    "engine": "2.0 Дизель",
+    "gearbox": "Автомат",
+    "mileage": 120000,
+    "mileageUnit": "km",
+    "srcImg": "img/lots/lot_22122019/lot_221219_01.jpg"
+},
+{
+    "id": 9,
+    "maker": "Ford",
+    "model": "Focus",
+    "price": 6000.00,
+    "currency": "EUR",
+    "year": 2013,
+    "colour": "Зелений",
+    "engine": "1.5 Дизель",
+    "gearbox": "Автомат",
+    "mileage": 199000,
+    "mileageUnit": "km",
+    "srcImg": "img/lots/lot_01012020/lot_01012020_01.jpg"
+},
+{
+    "id": 10,
+    "maker": "Jaguar",
+    "model": "XF",
+    "price": 33800.00,
+    "currency": "EUR",
+    "year": 2017,
+    "colour": "Сірий",
+    "engine": "2.0 Бензин",
+    "gearbox": "Автомат",
+    "mileage": 15400,
+    "mileageUnit": "km",
+    "srcImg": "img/lots/lot_02012020/lot_02012020_01.jpg"
+},
+{
+    "id": 11,
+    "maker": "Ford",
+    "model": "Ranger",
+    "price": 33471.00,
+    "currency": "EUR",
+    "year": 2017,
+    "colour": "Білий",
+    "engine": "3.2 Дизель",
+    "gearbox": "Механіка",
+    "mileage": 44300,
+    "mileageUnit": "km",
+    "srcImg": "img/lots/lot_03012020/lot_03012020_01.jpg"
+},
+{
+    "id": 12,
+    "maker": "Ford",
+    "model": "Kuga",
+    "price": 18736.00,
+    "currency": "EUR",
+    "year": 2016,
+    "colour": "Синій",
+    "engine": "2.0 Дизель",
+    "gearbox": "Автомат",
+    "mileage": 52446,
+    "mileageUnit": "km",
+    "srcImg": "img/lots/lot_04012020/lot_04012020_01.jpg"
+},
+{
+    "id": 13,
+    "maker": "Porsche",
+    "model": "Macan",
+    "price": 32600.00,
+    "currency": "EUR",
+    "year": 2016,
+    "colour": "Бежевий",
+    "engine": "2.0 Бензин",
+    "gearbox": "Автомат",
+    "mileage": 30700,
+    "mileageUnit": "km",
+    "srcImg": "img/lots/lot_05012020/lot_05012020_01.jpg"
+},
+{
+    "id": 14,
+    "maker": "Range Rover",
+    "model": "Evoque",
+    "price": 39350.00,
+    "currency": "EUR",
+    "year": 2019,
+    "colour": "Білий",
+    "engine": "2.0 Дизель",
+    "gearbox": "Автомат",
+    "mileage": 10410,
+    "mileageUnit": "km",
+    "srcImg": "img/lots/lot_06012020/lot_06012020_01.jpg"
+}
+]
+
+
+function lotsTemplate(lot) {
+    return `
+    <div class="auction__item">
+        <div class="auction__item-block">
+            <img class="auction__item-photo" src="${lot.srcImg}" alt="${lot.id}" title="${lot.id} - ${lot.maker} ${lot.model}">
+            <div class="auction__item-description">
+                <h2 class="auction__item-maker"><i></i>${lot.maker}</h2>
+                <h3 class="auction__item-model"><i></i>${lot.model}</h3>
+                <p class="lot__start-price"><i class="fas fa-money-bill"></i>${lot.price} ${lot.currency}</p>
+                <p class="lot__item-year"><i class="far fa-calendar-times"></i>${lot.year}</p>
+                <p class="lot__colour"><i class="fas fa-paint-brush"></i>${lot.colour}</p>
+                <p class="lot__gearbox"><i class="fas fa-cog"></i>${lot.gearbox}</p>
+                <p class="lot__engine"><i class="fas fa-gas-pump"></i>${lot.engine}</p>
+                <p class="lot__mileage"><i class="fas fa-tachometer-alt"></i>${lot.mileage} ${lot.mileageUnit}</p>
+                <input class="auction__item-details" type="submit" value="Деталі" onclick="window.location.href = 'lots_card.html';">
+            </div>
+        </div>
+    </div>
+    `
+  }
+  
+document.getElementById("auction__lots").innerHTML = `
+${lotsData.map(lotsTemplate).join('')}`
+
