eecf67573c5e992e537b4fa16115d386ab6b36a5
Nataliia Mogylchuk <nataliia.mogylchuk@gmail.com>
2020-01-11 18:47:39 +0200

    Add template for chosen lot

--- script/lots.js
+++ script/lots.js
@@ -1,201 +1,4 @@
-  const lotsData =  [{
-    "id": 1,
-    "maker": "Volvo",
-    "model": "V40",
-    "price": 19000.00,
-    "currency": "EUR",
-    "year": 2013,
-    "colour": "Синій",
-    "engine": "2.0 Дизель",
-    "gearbox": "Автомат",
-    "mileage": 120000,
-    "mileageUnit": "km",
-    "srcImg": "img/lots/lot_22122019/lot_221219_01.jpg"
-},
-{
-    "id": 2,
-    "maker": "Ford",
-    "model": "Focus",
-    "price": 6000.00,
-    "currency": "EUR",
-    "year": 2013,
-    "colour": "Зелений",
-    "engine": "1.5 Дизель",
-    "gearbox": "Автомат",
-    "mileage": 199000,
-    "mileageUnit": "km",
-    "srcImg": "img/lots/lot_01012020/lot_01012020_01.jpg"
-},
-{
-    "id": 3,
-    "maker": "Jaguar",
-    "model": "XF",
-    "price": 33800.00,
-    "currency": "EUR",
-    "year": 2017,
-    "colour": "Сірий",
-    "engine": "2.0 Бензин",
-    "gearbox": "Автомат",
-    "mileage": 15400,
-    "mileageUnit": "km",
-    "srcImg": "img/lots/lot_02012020/lot_02012020_01.jpg"
-},
-{
-    "id": 4,
-    "maker": "Ford",
-    "model": "Ranger",
-    "price": 33471.00,
-    "currency": "EUR",
-    "year": 2017,
-    "colour": "Білий",
-    "engine": "3.2 Дизель",
-    "gearbox": "Механіка",
-    "mileage": 44300,
-    "mileageUnit": "km",
-    "srcImg": "img/lots/lot_03012020/lot_03012020_01.jpg"
-},
-{
-    "id": 5,
-    "maker": "Ford",
-    "model": "Kuga",
-    "price": 18736.00,
-    "currency": "EUR",
-    "year": 2016,
-    "colour": "Синій",
-    "engine": "2.0 Дизель",
-    "gearbox": "Автомат",
-    "mileage": 52446,
-    "mileageUnit": "km",
-    "srcImg": "img/lots/lot_04012020/lot_04012020_01.jpg"
-},
-{
-    "id": 6,
-    "maker": "Porsche",
-    "model": "Macan",
-    "price": 32600.00,
-    "currency": "EUR",
-    "year": 2016,
-    "colour": "Бежевий",
-    "engine": "2.0 Бензин",
-    "gearbox": "Автомат",
-    "mileage": 30700,
-    "mileageUnit": "km",
-    "srcImg": "img/lots/lot_05012020/lot_05012020_01.jpg"
-},
-{
-    "id": 7,
-    "maker": "Range Rover",
-    "model": "Evoque",
-    "price": 39350.00,
-    "currency": "EUR",
-    "year": 2019,
-    "colour": "Білий",
-    "engine": "2.0 Дизель",
-    "gearbox": "Автомат",
-    "mileage": 10410,
-    "mileageUnit": "km",
-    "srcImg": "img/lots/lot_06012020/lot_06012020_01.jpg"
-},
-{
-    "id": 8,
-    "maker": "Volvo",
-    "model": "V40",
-    "price": 19000.00,
-    "currency": "EUR",
-    "year": 2013,
-    "colour": "Синій",
-    "engine": "2.0 Дизель",
-    "gearbox": "Автомат",
-    "mileage": 120000,
-    "mileageUnit": "km",
-    "srcImg": "img/lots/lot_22122019/lot_221219_01.jpg"
-},
-{
-    "id": 9,
-    "maker": "Ford",
-    "model": "Focus",
-    "price": 6000.00,
-    "currency": "EUR",
-    "year": 2013,
-    "colour": "Зелений",
-    "engine": "1.5 Дизель",
-    "gearbox": "Автомат",
-    "mileage": 199000,
-    "mileageUnit": "km",
-    "srcImg": "img/lots/lot_01012020/lot_01012020_01.jpg"
-},
-{
-    "id": 10,
-    "maker": "Jaguar",
-    "model": "XF",
-    "price": 33800.00,
-    "currency": "EUR",
-    "year": 2017,
-    "colour": "Сірий",
-    "engine": "2.0 Бензин",
-    "gearbox": "Автомат",
-    "mileage": 15400,
-    "mileageUnit": "km",
-    "srcImg": "img/lots/lot_02012020/lot_02012020_01.jpg"
-},
-{
-    "id": 11,
-    "maker": "Ford",
-    "model": "Ranger",
-    "price": 33471.00,
-    "currency": "EUR",
-    "year": 2017,
-    "colour": "Білий",
-    "engine": "3.2 Дизель",
-    "gearbox": "Механіка",
-    "mileage": 44300,
-    "mileageUnit": "km",
-    "srcImg": "img/lots/lot_03012020/lot_03012020_01.jpg"
-},
-{
-    "id": 12,
-    "maker": "Ford",
-    "model": "Kuga",
-    "price": 18736.00,
-    "currency": "EUR",
-    "year": 2016,
-    "colour": "Синій",
-    "engine": "2.0 Дизель",
-    "gearbox": "Автомат",
-    "mileage": 52446,
-    "mileageUnit": "km",
-    "srcImg": "img/lots/lot_04012020/lot_04012020_01.jpg"
-},
-{
-    "id": 13,
-    "maker": "Porsche",
-    "model": "Macan",
-    "price": 32600.00,
-    "currency": "EUR",
-    "year": 2016,
-    "colour": "Бежевий",
-    "engine": "2.0 Бензин",
-    "gearbox": "Автомат",
-    "mileage": 30700,
-    "mileageUnit": "km",
-    "srcImg": "img/lots/lot_05012020/lot_05012020_01.jpg"
-},
-{
-    "id": 14,
-    "maker": "Range Rover",
-    "model": "Evoque",
-    "price": 39350.00,
-    "currency": "EUR",
-    "year": 2019,
-    "colour": "Білий",
-    "engine": "2.0 Дизель",
-    "gearbox": "Автомат",
-    "mileage": 10410,
-    "mileageUnit": "km",
-    "srcImg": "img/lots/lot_06012020/lot_06012020_01.jpg"
-}
-]
-
+const lotsData = loadLotsData();
 
 function lotsTemplate(lot) {
     return `
@@ -211,13 +14,18 @@ function lotsTemplate(lot) {
                 <p class="lot__gearbox"><i class="fas fa-cog"></i>${lot.gearbox}</p>
                 <p class="lot__engine"><i class="fas fa-gas-pump"></i>${lot.engine}</p>
                 <p class="lot__mileage"><i class="fas fa-tachometer-alt"></i>${lot.mileage} ${lot.mileageUnit}</p>
-                <input class="auction__item-details" type="submit" value="Деталі" onclick="window.location.href = 'lots_card.html';">
+                <input class="auction__item-details" type="submit" value="Деталі" onclick="linkToChosenLot(${lot.id})"></button>
             </div>
         </div>
     </div>
     `
-  }
-  
+}
+
 document.getElementById("auction__lots").innerHTML = `
 ${lotsData.map(lotsTemplate).join('')}`
 
+function linkToChosenLot(lotId) {
+    var lot = lotsData.find(x => x.id === lotId);
+    localStorage.setItem("currentLot", JSON.stringify(lot));
+    window.location.href = 'lots_card.html';
+}
---
+++ script/lots_card.js
@@ -0,0 +1,118 @@
+function loadCurrentLot() {
+    var lot = JSON.parse(localStorage.getItem("currentLot"));
+    var imgSrcText = getImgSrcText(lot.images);
+    var imgDivText = getImgDivText(lot.images);
+    var interierLiText = getLiText(lot.interier);
+    var exteriorLiText = getLiText(lot.exterior);
+    var safatyLiText = getLiText(lot.safaty);
+    var descriptionLiText = getLiText(lot.description);
+    return `
+    <section>
+
+      <div class="lot__information">
+      <!-- Full size images -->
+        
+        <div class="lot__slider">
+          ${imgDivText}
+
+          <!-- Previous and next buttons -->
+          <a class="lot__slider-prev" onclick="plusSlides(-1)">&#10094;</a>
+          <a class="lot__slider-next" onclick="plusSlides(1)">&#10095;</a>
+
+          <!-- Mini size images -->
+
+          <div class="lot__slider-mini">
+            ${imgSrcText}
+          </div>
+        </div>
+
+        <div class="lot__details">
+          <h2 class="lot__model">${lot.maker} ${lot.model}</h2>
+          <p id="lot__vin">${lot.vin}</p>
+          <div class="lot__details-item">
+            <p class="lot__start-price"><i class="fas fa-money-bill"></i>${lot.price} ${lot.currency}</p>
+            <p class="lot__item-year"><i class="far fa-calendar-times"></i>${lot.year}</p>
+            <p class="lot__colour"><i class="fas fa-paint-brush"></i>${lot.colour}</p>
+            <p class="lot-engine"><i class="fas fa-gas-pump"></i>${lot.engine}</p>
+            <p class="lot-gearbox"><i class="fas fa-cog"></i>${lot.gearbox}</p>
+            <p class="lot-mileage"><i class="fas fa-tachometer-alt"></i>${lot.mileage} ${lot.mileageUnit}</p>
+          </div>
+        </div>
+
+        <div class="lot__bid-information">
+          <h2>Інформація по ставці</h2>
+          <p class="lot__bid-start">Стартова ціна:</p>
+          <p class="lot__bid-current">Поточна ціна:</p>
+          <input class="lot__bid-fee" type="submit" value="Зробити ставку">
+        </div>
+      </div>
+    </section>
+
+    <section>
+      <!-- Lot description -->
+      <div class="lot__description">
+        <div class="lot__description-item">
+          <h2>Інтер'єр</h2>
+          <ul class="lot__interior">
+            ${interierLiText}
+          </ul>
+        </div>
+
+        <div class="lot__description-item">
+          <h2>Екстер'єр</h2>
+          <ul class="lot__exterior">
+            ${exteriorLiText}
+          </ul>
+        </div>
+
+        <div class="lot__description-item">
+          <h2>Безпека</h2>
+          <ul class="lot__safety">
+            ${safatyLiText}
+          </ul>
+        </div>
+
+        <div class="lot__description-item">
+          <h2>Інше</h2>
+          <ul class="lot__other">
+            ${descriptionLiText}
+          </ul>
+        </div>
+      </div>
+    </section>
+    `
+}
+
+function getImgSrcText(imgArray) {
+    var imgText = "";
+    if (imgArray !== null) {
+        for (i = 0; i < imgArray.length; i++) {
+            imageId = i + 1;
+            imgText += `<img class="lotSliderMiniItem" src="${imgArray[i]}" alt="lot__slider-mini${imageId}" onclick="currentSlide(${imageId})"></img>\n`;
+        }
+    }
+    return imgText;
+}
+
+function getImgDivText(imgArray) {
+    var imgText = "";
+    if (imgArray !== null) {
+        for (i = 0; i < imgArray.length; i++) {
+            imageId = i + 1;
+            imgText += `<div class="lotSliderMaxi"><div class="lot__slider-number">${imageId} / ${imgArray.length}</div><img src="${imgArray[i]}" alt="lot__slider-maxi1"></div>\n`;
+        }
+    }
+    return imgText;
+}
+
+function getLiText(textArray) {
+    var liText = "";
+    if (textArray !== null) {
+        for (i = 0; i < textArray.length; i++) {
+            liText += `<li>${textArray[i]}</li>\n`;
+        }
+    }
+    return liText;
+}
+
+document.getElementById("lot__fullinformation").innerHTML = `${loadCurrentLot()}`
